72102457084408caed70e66d35fc6941c49b25b5
Chinmay Bhoir <chinmay.bhoir@atmecs.com>
2017-09-14 18:08:57 +0530

    Refactoring

--- texteditor.py
+++ texteditor.py
@@ -10,70 +10,77 @@ import tkinter.ttk as ttk
 
 FILELOCATION = ""
 
-def save_as():
+def save_as(text):
     """ Save new file """
-    global TEXT
-    t = TEXT.get("1.0", "end-1c")
+    t = text.get("1.0", "end-1c")
     save_location = fdialogue.asksaveasfilename()
-    new_file = open(save_location, 'w+')
-    new_file.write(t)
-    new_file.close()
+    newfile = open(save_location, 'w+')
+    newfile.write(t)
+    newfile.close()
 
-def already_open():
+def already_open(text):
     """ Check if another file is already opened in buffer
         Used for opening another file if a file is already opened in buffer
     """
-    global TEXT
-    text = TEXT.get("1.0", "end-1c")
-    if len(text) == 0:
+    filetext = text.get("1.0", "end-1c")
+    if len(filetext) == 0:
         return False
     return True
 
-def save_file():        
+def save_file(text):        
     """ Save the file """
-    global TEXT
-    text = TEXT.get("1.0", "end-1c")
+    text = text.get("1.0", "end-1c")
     global FILELOCATION
     # Check if the file in current buffer is new
     if len(FILELOCATION) == 0:
-        save_as()
+        save_as(text)
     else:
         opened_file = open(FILELOCATION, 'w')
         opened_file.write(text)
 
-def open_file():
+def new_file():
+    """ Create new file """
+    
+
+def open_file(text):
     """ Open new file in editor """
     global FILELOCATION
     file_location = fdialogue.askopenfilename()
     opened_file = open(file_location)
-    text = [line for line in opened_file]
-    text_string = "".join(text)
-    global TEXT
-    if already_open():
-        TEXT.delete("1.0", "end-1c")
-    TEXT.insert("1.0", text_string)
+    temt = [line for line in opened_file]
+    text_string = "".join(temt)
+    if already_open(text):
+        text.delete("1.0", "end-1c")
+    text.insert("1.0", text_string)
     opened_file.close()
     FILELOCATION = file_location
 
+def app():
+    root = tk.Tk()
+    main_frame = tk.Frame(root)
+    main_frame.master.title("Text Editor")
+    main_frame.configure(background=TOOLBAR_BGCOLOR)
+    main_frame.pack(fill=tk.BOTH, expand=True)
+    nav_bar = tk.Canvas(main_frame)
+    nav_bar.pack(side=tk.TOP)
+    text = tk.Text(main_frame)
+    text.configure(background=EDITOR_BGCOLOR)
+    text.pack(fill=tk.BOTH, expand=True)
+    newfile_button = tk.Button(nav_bar, text="New", command=lambda: new_file(text))
+    newfile_button.configure(background=BUTTONS_BGCOLOR)
+    newfile_button.pack(side=tk.LEFT)
+    saveas_button = tk.Button(nav_bar, text="Save As", command=lambda: save_as(text))
+    saveas_button.configure(background=BUTTONS_BGCOLOR)
+    saveas_button.pack(side=tk.LEFT)
+    save_button = tk.Button(nav_bar, text="Save", command=lambda: save_file(text))
+    save_button.configure(background=BUTTONS_BGCOLOR)
+    save_button.pack(side=tk.LEFT)
+    open_button = tk.Button(nav_bar, text="Open", command=lambda: open_file(text))
+    open_button.configure(background=BUTTONS_BGCOLOR)
+    open_button.pack(side=tk.LEFT)
+    return root    
+
 if __name__ == '__main__':
     """ This is master	branch """
-    ROOT = tk.Tk()
-    MAIN_FRAME = tk.Frame(ROOT)
-    MAIN_FRAME.master.title("Text Editor")
-    MAIN_FRAME.configure(background=TOOLBAR_BGCOLOR)
-    MAIN_FRAME.pack(fill=tk.BOTH, expand=True)
-    NAV_BAR = tk.Canvas(MAIN_FRAME)
-    NAV_BAR.pack(side=tk.TOP)
-    TEXT = tk.Text(MAIN_FRAME)
-    TEXT.configure(background=EDITOR_BGCOLOR)
-    TEXT.pack(fill=tk.BOTH, expand=True)
-    SAVEAS_BUTTON = tk.Button(NAV_BAR, text="Save As", command=save_as)
-    SAVEAS_BUTTON.configure(background=BUTTONS_BGCOLOR)
-    SAVEAS_BUTTON.pack(side=tk.LEFT)
-    SAVE_BUTTON = tk.Button(NAV_BAR, text="Save", command=save_file)
-    SAVE_BUTTON.configure(background=BUTTONS_BGCOLOR)
-    SAVE_BUTTON.pack(side=tk.LEFT)
-    OPEN_BUTTON = tk.Button(NAV_BAR, text="Open", command=open_file)
-    OPEN_BUTTON.configure(background=BUTTONS_BGCOLOR)
-    OPEN_BUTTON.pack(side=tk.LEFT)
+    ROOT = app()
     ROOT.mainloop()
